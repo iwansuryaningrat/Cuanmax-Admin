923ce0ca360006a1b2c0e05f17d8f94855f11a5e
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-29 10:15:59 +0700

    Create Layout Templates

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="pull-left">
+
+        </nav>
+        <div class="copyright ml-auto">
+            2022, made with <i class="fa fa-heart heart text-danger"></i> by <strong>Cuanmax Developer</strong>
+        </div>
+    </div>
+</footer>

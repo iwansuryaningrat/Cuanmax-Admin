84c9c8deada705588f4d3fcc9eb51e5edb2bfcfd
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-29 11:05:00 +0700

    Nice Try!!

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,18 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+    public function __construct()
+    {
+        $this->client = \Config\Services::curlrequest([
+            'baseURI' => 'http://localhost:3000/api/v1/',
+        ]);
+    }
     public function index()
     {
-        return view('index');
+        $result = $this->client->get('cryptocurrency/latest');
+        $data = json_decode($result->getBody(), true);
+        $status = $result->getStatusCode();
+
+        dd($status, $data);
     }
 }

9ede1d74882b8dcaa7d38ef00bb2f4b1cbe9c50c
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-29 12:05:10 +0700

    Update Home Controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,13 +6,21 @@ class Home extends BaseController
 {
     public function __construct()
     {
+        // Curl Request Config
         $this->client = \Config\Services::curlrequest([
             'baseURI' => 'http://localhost:3000/api/v1/',
         ]);
+
+        // Get session data
+
     }
     public function index()
     {
-        $result = $this->client->get('cryptocurrency/latest');
+        $result = $this->client->get('cryptocurrency/info', [
+            'query' => [
+                'symbol' => 'BNB',
+            ],
+        ]);
         $data = json_decode($result->getBody(), true);
         $status = $result->getStatusCode();
 

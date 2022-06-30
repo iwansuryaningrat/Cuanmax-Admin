041ad3cb3cab562d1f6337d16b4e0203234e19e3
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-30 10:52:15 +0700

    Update Users Controller

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -6,6 +6,17 @@ use App\Controllers\BaseController;
 
 class Users extends BaseController
 {
+    public function __construct()
+    {
+        // Curl Request Config
+        $this->client = \Config\Services::curlrequest([
+            'baseURI' => 'http://localhost:3000/api/v1/',
+        ]);
+
+        // Get session data
+
+    }
+
     public function index()
     {
         //

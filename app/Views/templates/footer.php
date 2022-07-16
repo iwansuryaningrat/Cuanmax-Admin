ad3dbaa1da24afa92a72b8de1ad27958752cc234
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-16 20:14:12 +0700

    Make Modular Layout Template

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,7 +1,13 @@
 <footer class="footer">
     <div class="container-fluid">
         <nav class="pull-left">
-
+            <ul class="nav">
+                <li class="nav-item">
+                    <a>
+                        Version 1.0.0
+                    </a>
+                </li>
+            </ul>
         </nav>
         <div class="copyright ml-auto">
             2022, made with <i class="fa fa-heart heart text-danger"></i> by <strong>Cuanmax Developer</strong>

bea40427346ed6e25d55786f4ff53c884742f7a2
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-16 20:07:57 +0700

    Update Dashboard Content Layout

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,23 +7,24 @@ class Home extends BaseController
     public function __construct()
     {
         // Curl Request Config
-        $this->client = \Config\Services::curlrequest([
-            'baseURI' => 'http://localhost:3000/api/v1/',
-        ]);
+        // $this->client = \Config\Services::curlrequest([
+        //     'baseURI' => 'http://localhost:3000/api/v1/',
+        // ]);
 
         // Get session data
 
     }
     public function index()
     {
-        $result = $this->client->get('cryptocurrency/info', [
-            'query' => [
-                'symbol' => 'BNB',
-            ],
-        ]);
-        $data = json_decode($result->getBody(), true);
-        $status = $result->getStatusCode();
+        // $result = $this->client->get('cryptocurrency/info', [
+        //     'query' => [
+        //         'symbol' => 'BNB',
+        //     ],
+        // ]);
+        // $data = json_decode($result->getBody(), true);
+        // $status = $result->getStatusCode();
 
-        dd($status, $data);
+        // dd($status, $data);
+        return view('index');
     }
 }
